7c1858107d12398309cd5f93aa78f81ecb7e49ba
DrYoshiyahu <josiahhilbig@gmail.com>
2020-12-03 21:49:31 +1100

    Added Object.keys polyfill
    I don't even know how many compatibility issues I've found tonight, and at this point, I'm afraid to ask.

--- script.js
+++ script.js
@@ -101,6 +101,48 @@ if (!document.getElementsByClassName) {
 	};
 }
 
+//Object.keys polyfill
+if (!Object.keys) {
+	Object.keys = (function() {
+		'use strict';
+		var hasOwnProperty = Object.prototype.hasOwnProperty,
+		hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+		dontEnums = [
+		'toString',
+		'toLocaleString',
+		'valueOf',
+		'hasOwnProperty',
+		'isPrototypeOf',
+		'propertyIsEnumerable',
+		'constructor'
+		],
+		dontEnumsLength = dontEnums.length;
+
+		return function(obj) {
+			if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+				throw new TypeError('Object.keys called on non-object');
+			}
+
+			var result = [], prop, i;
+
+			for (prop in obj) {
+				if (hasOwnProperty.call(obj, prop)) {
+					result.push(prop);
+				}
+			}
+
+			if (hasDontEnumBug) {
+				for (i = 0; i < dontEnumsLength; i++) {
+					if (hasOwnProperty.call(obj, dontEnums[i])) {
+						result.push(dontEnums[i]);
+					}
+				}
+			}
+			return result;
+		};
+	}());
+}
+
 function settings() { //Shows and hides the settings.
 	var settings = document.getElementById("settings");
 	if (settings.style.display === "block") {
