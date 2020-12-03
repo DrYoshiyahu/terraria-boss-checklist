efd017a448125194bdfefc0d9d2d24af9dd1858e
DrYoshiyahu <josiahhilbig@gmail.com>
2020-12-03 21:28:51 +1100

    Added a new, different addEventListener polyfill that didn't require changing other functions. (Are we there yet?)

--- script.js
+++ script.js
@@ -43,23 +43,34 @@ if (!document.querySelector) {
 }
 
 //addEventListener polyfill
-window.addEvent = function(obj, type, fn) {
-	if (obj.addEventListener) {
-		obj.addEventListener(type, fn, false);
-	} else if (obj.attachEvent) {
-		obj["e" + type + fn] = fn;
-		obj[type + fn] = function() {
-			var e = window.event;
-			e.currentTarget = obj;
-			e.preventDefault = function() { e.returnValue = false; };
-			e.stopPropagation = function() { e.cancelBubble = true; };
-			e.target = e.srcElement;
-			e.timeStamp = Date.now();
-			obj["e" + type + fn].call(this, e);
-		};
-		obj.attachEvent("on" + type, obj[type + fn]);
+(function(win, doc){
+	if(win.addEventListener)return;	//No need to polyfill
+
+	function docHijack(p){var old = doc[p];doc[p] = function(v){return addListen(old(v))}}
+	function addEvent(on, fn, self){
+		return (self = this).attachEvent('on' + on, function(e){
+			var e = e || win.event;
+			e.preventDefault  = e.preventDefault  || function(){e.returnValue = false}
+			e.stopPropagation = e.stopPropagation || function(){e.cancelBubble = true}
+			fn.call(self, e);
+		});
 	}
-};
+	function addListen(obj, i){
+		if(i = obj.length)while(i--)obj[i].addEventListener = addEvent;
+		else obj.addEventListener = addEvent;
+		return obj;
+	}
+
+	addListen([doc, win]);
+	if('Element' in win)win.Element.prototype.addEventListener = addEvent; //IE8
+	else{ //IE < 8
+		doc.attachEvent('onreadystatechange', function(){addListen(doc.all)}); //Make sure we also init at domReady
+		docHijack('getElementsByTagName');
+		docHijack('getElementById');
+		docHijack('createElement');
+		addListen(doc.all);	
+	}
+})(window, document);
 
 //forEach polyfill
 if (!Array.prototype['forEach']) {
